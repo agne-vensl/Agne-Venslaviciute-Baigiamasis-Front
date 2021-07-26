aea992d307f890efdb6c35910d93f2ce7fb2a8cf
Agnė Venslavičiūtė <73463827+agne-vensl@users.noreply.github.com>
2021-07-26 16:30:12 +0300

    scrollbar, font size and line height style updates

--- src/components/Window/Window.style.js
+++ src/components/Window/Window.style.js
@@ -3,25 +3,38 @@ import styled from 'styled-components';
 export const WindowContainer = styled.div`
   background: transparent;
   color: #d5d5d5;
+  font-size: 0.8rem;
   height: ${(props) => props.height};
+  line-height: 1.1rem;
   margin: 0.75rem 0;
   overflow-y: scroll;
   padding: 0 0.7rem;
   padding-top: 0;
   width: ${(props) => props.width};
+  word-wrap: break-word;
 
   &::-webkit-scrollbar {
-    width: 1rem;
+    display: none;
   }
 
-  &::-webkit-scrollbar-track {
-    background: #222;
-    border-radius: 0.5rem;
-  }
+  @media only screen and (min-width: 768px) {
+    font-size: 1rem;
+    line-height: 1.3rem;
 
-  ::-webkit-scrollbar-thumb {
-    background: #555;
-    border-radius: 0.5rem;
+    &::-webkit-scrollbar {
+      display: initial;
+      width: 1rem;
+    }
+
+    &::-webkit-scrollbar-track {
+      background: #222;
+      border-radius: 0.5rem;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: #555;
+      border-radius: 0.5rem;
+    }
   }
 `;
 
