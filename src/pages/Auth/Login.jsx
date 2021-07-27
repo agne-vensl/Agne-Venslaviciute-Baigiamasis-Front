ff8be173c55276aaf22676175d3bf8c3148e9306
Agnė Venslavičiūtė <73463827+agne-vensl@users.noreply.github.com>
2021-07-27 22:03:28 +0300

    login page

---
+++ src/pages/Auth/Login.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import { Button, Form } from '../../components';
+
+const Login = ({ closeModal }) => {
+  const [formData] = useState({ email: '', password: '' });
+
+  const login = (e) => {
+    e.preventDefault();
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(formData.email, formData.password)
+      .then(() => {
+        closeModal();
+      })
+      .catch((er) => alert(er.message));
+  };
+
+  return (
+    <Form handleSubmit={login}>
+      <h1>Login</h1>
+
+      <label htmlFor="email">
+        Email
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="email@email.com"
+          minLength="8"
+          required
+          onChange={(e) => {
+            formData.email = e.target.value;
+          }}
+        />
+      </label>
+
+      <label htmlFor="password">
+        Password
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="password"
+          minLength="4"
+          required
+          onChange={(e) => {
+            formData.password = e.target.value;
+          }}
+        />
+      </label>
+
+      <Button text="Login" color="primary" type="submit" />
+    </Form>
+  );
+};
+
+Login.propTypes = {
+  closeModal: PropTypes.func,
+};
+
+Login.defaultProps = {
+  closeModal: null,
+};
+
+export default Login;
