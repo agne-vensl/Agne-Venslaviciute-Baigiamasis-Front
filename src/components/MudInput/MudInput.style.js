3af06e5e3be0953c31c9b8887b5a60ba8ff3a8f8
Agnė Venslavičiūtė <73463827+agne-vensl@users.noreply.github.com>
2021-07-27 16:20:02 +0300

    line height, font size, padding updates

--- src/components/MudInput/MudInput.style.js
+++ src/components/MudInput/MudInput.style.js
@@ -9,12 +9,15 @@ export const Input = styled.input`
   color: #eee;
   border: 0;
   font-size: 0.8rem;
+  line-height: 0.85rem;
   outline: none;
-  padding: 0.6rem;
+  padding: 0.3rem;
   width: 100%;
   flex: 1;
 
   @media only screen and (min-width: 768px) {
+    font-size: 0.9rem;
+    line-height: initial;
     padding: 0.7rem;
   }
 `;
@@ -27,5 +30,9 @@ export const Button = styled.button`
   cursor: pointer;
   margin: 0.2rem 0.2rem 0.2rem 0;
   outline: none;
-  padding: 0.5rem 1rem;
+  padding: 0.3rem 0.7rem;
+
+  @media only screen and (min-width: 768px) {
+    padding: 0.5rem 1rem;
+  }
 `;
