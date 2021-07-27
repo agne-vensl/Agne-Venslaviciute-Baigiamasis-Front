29be42e4354dd3d4e30cc59566524c8c91c6b839
Agnė Venslavičiūtė <73463827+agne-vensl@users.noreply.github.com>
2021-07-27 22:04:08 +0300

    form and button exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,5 @@
+export { default as Button } from './Button/Button';
+export { default as Form } from './Form/Form';
 export { default as ModalContainer } from './ModalContainer/ModalContainer';
 export { default as Menu } from './Menu/Menu';
 export { default as MudInput } from './MudInput/MudInput';
