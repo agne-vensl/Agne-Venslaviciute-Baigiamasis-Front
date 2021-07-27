0956326afe301d5009ca8c1e2a7eb62b3f282e7f
Agnė Venslavičiūtė <73463827+agne-vensl@users.noreply.github.com>
2021-07-27 16:43:33 +0300

    import fix

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { ModalContainer } from '..';
+import ModalContainer from '../ModalContainer/ModalContainer';
 
 import * as S from './Menu.style';
 
