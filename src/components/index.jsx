21d04ffded8e73c70f5346d17eb20f9348ffe1b1
Agnė Venslavičiūtė <73463827+agne-vensl@users.noreply.github.com>
2021-07-27 16:43:21 +0300

    menu and modal container exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,4 @@
+export { default as ModalContainer } from './ModalContainer/ModalContainer';
+export { default as Menu } from './Menu/Menu';
 export { default as MudInput } from './MudInput/MudInput';
 export { default as Window } from './Window/Window';
